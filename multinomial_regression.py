b0ff8bc17e480b486a94ca6b27e29bee5293da8f
hannariss <rissbacher.h@gmail.com>
2025-10-13 16:01:11 +0200

    add(multinomial_regression)

---
+++ multinomial_regression.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+stress_data = pd.read_csv('data/Stress_Dataset.csv')
+
+stress_data.rename(columns={'Which type of stress do you primarily experience?': 'target'}, inplace=True)
+label_encoder = sk.preprocessing.LabelEncoder()
+
+# Fit and transform
+stress_data['target_encoded'] = label_encoder.fit_transform(stress_data['target'])
+
+# define model
+multi_reg = LogisticRegression(solver='newton-cg', max_iter=1000)
+
+X = stress_data.drop(columns=['target', 'target_encoded'])
+y = stress_data['target_encoded']
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+multi_reg.fit(X_train, y_train)
+pred_y = multi_reg.predict(X_test)
+
+# inspect coefficients
+classes = multi_reg.classes_
+# Coefficients (one row per class)
+coef_df = pd.DataFrame(multi_reg.coef_, columns=X.columns, index=classes) 
+#column shows how strongly that predictor affects the odds of belonging to that class.
